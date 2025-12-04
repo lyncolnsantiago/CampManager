d2113e3a1991449acc9bdeb5cdea4b002d3c8d60
lyncolnsantiago <lyncolnsantiago7@gmail.com>
2025-12-04 15:54:06 -0300

    Adicionar projeto Spring Boot

---
+++ src/main/java/br/com/campmanager/projeto/CampManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.campmanager.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/campmanager/projeto/CampManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.campmanager.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
